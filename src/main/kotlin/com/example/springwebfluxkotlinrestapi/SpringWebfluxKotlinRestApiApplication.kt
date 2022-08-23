12e08301ab08c6e81e59b70ae006734fb80ae616
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-08-23 20:06:11 -0300

    First commit

---
+++ src/main/kotlin/com/example/springwebfluxkotlinrestapi/SpringWebfluxKotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springwebfluxkotlinrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxKotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxKotlinRestApiApplication>(*args)
+}
